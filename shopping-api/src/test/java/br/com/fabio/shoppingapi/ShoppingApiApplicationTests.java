f238998a1aafbc9a4b0bc9b32c3af063291e71f0
Fabio Damas <fabio.damas@gmail.com>
2021-04-25 17:44:18 -0300

    commit inicial do shopping api

---
+++ shopping-api/src/test/java/br/com/fabio/shoppingapi/ShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabio.shoppingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
