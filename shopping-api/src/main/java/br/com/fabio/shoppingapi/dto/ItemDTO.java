f3cd929dd30352007116a6da1135b18d9dba6573
Fabio Damas <fabio.damas@gmail.com>
2021-04-25 22:21:43 -0300

    Criação do shopping

---
+++ shopping-api/src/main/java/br/com/fabio/shoppingapi/dto/ItemDTO.java
@@ -0,0 +1,39 @@
+package br.com.fabio.shoppingapi.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import br.com.fabio.shoppingapi.model.Item;
+
+public class ItemDTO {
+    @NotBlank
+    private String productIdentifier;
+    @NotNull
+    private Float price;
+
+
+
+
+    public String getProductIdentifier() {
+        return productIdentifier;
+    }
+
+    public void setProductIdentifier(String productIdentifier) {
+        this.productIdentifier = productIdentifier;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    // get and sets
+    public static ItemDTO convert(Item item) {
+        ItemDTO itemDTO = new ItemDTO();
+        itemDTO.setProductIdentifier(item.getProductIdentifier());
+        itemDTO.setPrice(item.getPrice());
+        return itemDTO;
+    }
+}
---
+++ shopping-api/src/main/java/br/com/fabio/shoppingapi/dto/ShopDTO.java
@@ -0,0 +1,60 @@
+package br.com.fabio.shoppingapi.dto;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.com.fabio.shoppingapi.model.Shop;
+
+public class ShopDTO {
+    @NotBlank
+    private String userIdentifier;
+    @NotNull
+    private Float total;
+    @NotNull
+    private Date date;
+    @NotNull
+    private List<ItemDTO> items;
+
+    public String getUserIdentifier() {
+        return userIdentifier;
+    }
+
+    public void setUserIdentifier(String userIdentifier) {
+        this.userIdentifier = userIdentifier;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<ItemDTO> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemDTO> items) {
+        this.items = items;
+    }
+
+    // get and sets
+    public static ShopDTO convert(Shop shop) {
+        ShopDTO shopDTO = new ShopDTO();
+        shopDTO.setUserIdentifier(shop.getUserIdentifier());
+        shopDTO.setTotal(shop.getTotal());
+        return shopDTO;
+    }
+}
